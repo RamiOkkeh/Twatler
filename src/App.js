aae9654ce49feff7cfbd31f027a258f1d8028713
RamiOkkeh <ramiokkeh@gmail.com>
2021-02-09 14:02:12 +0200

    created nav and home, also set up react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Nav from "./components/Nav";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Nav />
-    </div>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,8 @@
+function Home() {
+  return (
+    <div>
+      <div>home</div>
+    </div>
+  );
+}
+export default Home;
