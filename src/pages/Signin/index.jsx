b0d32610be5bac33fbdca31220706ba30f7310e0
RamiOkkeh <ramiokkeh@gmail.com>
2021-02-16 15:21:10 +0200

    jquery

--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -1,4 +1,29 @@
 function Signin() {
-  return <div></div>;
+  const submit = (e) => {
+    console.log(e);
+    e.preventDefault();
+  };
+  return (
+    <div className="signupCon">
+      <div className="signCover">
+        <img
+          className="logo"
+          src="https://www.lter-europe.net/document-archive/image-gallery/albums/logos/TwitterLogo_55acee.png/image"
+          alt="logo"
+        />
+      </div>
+      <form className="formCon">
+        <div>
+          <label name="email">Email</label>
+          <input type="text" name="email" />
+        </div>
+        <div>
+          <label name="password">Password</label>
+          <input type="text" name="password" />
+        </div>
+        <button onClick={submit}>Login</button>
+      </form>
+    </div>
+  );
 }
 export default Signin;
--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -1,9 +1,39 @@
 import "./Signup.css";
 
 function Signup() {
+  const submit = (e) => {
+    console.log(e);
+    e.preventDefault();
+  };
+
   return (
     <div className="signupCon">
-      <div className="signCover"></div>
+      <div className="signCover">
+        <img
+          className="logo"
+          src="https://www.lter-europe.net/document-archive/image-gallery/albums/logos/TwitterLogo_55acee.png/image"
+          alt="logo"
+        />
+      </div>
+      <form className="formCon">
+        <div>
+          <label name="username">Username</label>
+          <input type="text" name="username" />
+        </div>
+        <div>
+          <label name="nickname">Nickname</label>
+          <input type="text" name="nickname" />
+        </div>
+        <div>
+          <label name="email">Email</label>
+          <input type="text" name="email" />
+        </div>
+        <div>
+          <label name="password">Password</label>
+          <input type="text" name="password" />
+        </div>
+        <button onClick={submit}>Signup</button>
+      </form>
     </div>
   );
 }
